327d422afe9a7348ec641e17a830be4f9f922cce
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-06-21 23:14:33 +0900

    fixed value zoomFactor in PCapp

--- pcapp/app.js
+++ pcapp/app.js
@@ -1,13 +1,15 @@
+var path = require('path');
 const {app, BrowserWindow} = require('electron')
   function createWindow () {
     win = new BrowserWindow({
         width: 360, height: 640,
         webPreferences: {
-            zoomFactor: 3.0,
-            nodeIntegration: false
+            nodeIntegration: true,
+            preload: path.resolve(path.join(__dirname, 'preload.js'))
         }
     });
     win.setMenu(null);
     win.loadFile('./index.html');
+    win.webContents.session.clearCache(function(){});
   }
   app.on('ready', createWindow);
